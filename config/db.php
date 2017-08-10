9c7debca67fc73a382cd3fc1f5f56a9f079b39ae
umar0v <timur.r.umarov@gmail.com>
2017-08-10 22:32:19 +0300

    Изменяет файл конфигурации для базы данных

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@ $config = parse_ini_file('app-config.ini', true);
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=mysql;dbname=' . $config['mysql-db'],
+    'dsn' => 'mysql:host='. $config['mysql-host'] .';dbname=' . $config['mysql-db'],
     'username' => $config['mysql-user'],
     'password' => $config['mysql-pwd'],
     'charset' => 'utf8',
