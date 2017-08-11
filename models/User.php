dcbb85ba9dc41bce77b679f60660f0e7f9d84f26
Eldaen <sv_skatch@bk.ru>
2017-08-11 17:15:07 +0300

    Теперь нормально грузится модель User

--- models/User.php
+++ models/User.php
@@ -4,16 +4,12 @@ namespace app\models;
 
 class User extends \mdm\admin\models\searchs\User
 {
-    public $id;
-    public $username;
-    public $password;
-    public $password_hash;
-    public $accessToken;
-    public $status;
-    public $email;
-    public $auth_key;
-    public $created_at;
-    public $updated_at;
-    public $password_reset_token;
-
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
 }
+
+
+
+
