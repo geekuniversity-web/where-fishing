b0d7a7a756ebac55c6e9a3d6033f200c337c1883
Aleksey Pivovarov <texpiv@gmail.com>
2017-08-20 22:34:34 +0300

    В вёрстке введена возможность добавлять комментарии

---
+++ web/js/add-comment.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.otherOpinions__add-comment').click(function () {
+        $('.otherOpinions__add-comment').removeClass('otherOpinions__add-comment_active');
+        $('.place-page-form').addClass('place-page-form_active');
+    });
+});
